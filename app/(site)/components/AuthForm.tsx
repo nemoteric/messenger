2dbe0eb2391ff28965b3d7be55bd14ec2a7b262b
Eric John <43348893+nemoteric@users.noreply.github.com>
2023-05-31 08:35:43 -0500

    add prisma

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,20 +2,23 @@
 
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 import Input from '@/app/components/inputs/Input';
+import Button from '@/app/components/Button';
+import AuthSocialButton from './AuthSocialButton';
 
-type Varient = 'LOGIN' | 'REGISTER';
+type variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
-  const [varient, setVarient] = useState<Varient>('LOGIN');
+  const [variant, setvariant] = useState<variant>('LOGIN');
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const toggleVarient = useCallback(() => {
-    if (varient === 'LOGIN') {
-      setVarient('REGISTER');
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setvariant('REGISTER');
     } else {
-      setVarient('LOGIN');
+      setvariant('LOGIN');
     }
-  }, [varient]);
+  }, [variant]);
 
   const {
     register,
@@ -32,11 +35,11 @@ const AuthForm = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-    if (varient === 'REGISTER') {
+    if (variant === 'REGISTER') {
       // Axios Register
     }
 
-    if (varient === 'LOGIN') {
+    if (variant === 'LOGIN') {
       // NextAuth Login
     }
   };
@@ -63,8 +66,91 @@ const AuthForm = () => {
             sm:px-10
         '>
         <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
-          <Input label="Email"/>
+          {variant === 'REGISTER' && (
+            <Input 
+            id='name' 
+            label='Name' 
+            register={register} 
+            errors={errors}
+            disabled={isLoading} />
+          )}
+          <Input
+            id='email'
+            label='Email'
+            type='email'
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id='password'
+            label='Password'
+            type='password'
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type='submit'>
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
         </form>
+        <div className='mt-6'>
+          <div className='relative'>
+            <div
+              className='
+                    absolute
+                    inset-0
+                    flex
+                    items-center
+                  '>
+              <div className='w-full border-t border-gray-300'></div>
+            </div>
+            <div
+              className='relative 
+            flex 
+            justify-center 
+            text-sm'>
+              <span
+                className='
+                bg-white 
+                px-2 
+                text-gray-500'>
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className='mt-6 flex gap-2'>
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+        <div
+          className='
+          flex
+          gap-2
+          justify-center
+          text-sm
+          mt-6
+          px-2
+          text-gray-500
+          '>
+          <div>
+            {variant === 'LOGIN'
+              ? 'New to Messenger?'
+              : 'Already have an account?'}
+          </div>
+          <div onClick={toggleVariant} className='underline cursor-pointer'>
+            {variant === 'LOGIN' ? 'Create an account' : 'Sign in'}
+          </div>
+        </div>
       </div>
     </div>
   );
