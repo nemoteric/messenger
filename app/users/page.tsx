7f791c6d74430a0b8a8c1597536930103f9ba8df
Eric John <43348893+nemoteric@users.noreply.github.com>
2023-06-01 03:56:50 -0500

    add components

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,11 @@
-'use client';
+import EmptyState from '../components/EmptyState';
 
-import { signOut } from 'next-auth/react';
-
-const // This code is responsible for signing the user out of the application. It is called when the user clicks the "Log Out" button in the Users component. The signOut function is defined in the auth.js module, which is imported at the top of this file. The signOut function is responsible for removing the user's session from the server and then redirecting the user to the Sign In page.
-
-  Users = () => {
-    return <button onClick={() => signOut()}>Log Out</button>;
-  };
+const Users = () => {
+  return (
+    <div className='hidden lg:block lg:pl-80 h-full'>
+      <EmptyState />
+    </div>
+  );
+};
 
 export default Users;
